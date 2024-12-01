fc5c9e9d0a56d37f3c2f7502f3cf9caf521345b1
Vuhieu123 <hieuvutrong281@gmail.com>
2024-12-01 20:51:42 +0700

    first commit

---
+++ src/main/java/com/bookwormstore/BookwormStoreApplication.java
@@ -0,0 +1,13 @@
+package com.bookwormstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwormStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookwormStoreApplication.class, args);
+    }
+
+}
